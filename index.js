824871ebc59b0916bba4d5100cd5bc8b1d2ed8ca
Daniel <dannyb829@gmail.com>
2022-02-25 13:07:44 -0500

    commit tree 028d43d3bf7e56c37ff81ebd23cc9d7f2d7f2f77

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+// const findMatching = (arr, () => {return arr.filter(driver => driver['name'] = person)})
+
+function findMatching(arr, person) {
+    return arr.filter(driver => driver.toUpperCase() === person.toUpperCase())
+}
+
+const fuzzyMatch = (arr, char) => arr.filter(driver => driver.slice(0, char.length) === char)
+
+const matchName = (arr, person) => arr.filter(driver => driver.name === person)
+
+
+
+
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ]
+// const bobDrivers = drivers.filter(driver => driver.name === "Bobby")
